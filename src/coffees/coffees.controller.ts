13341e9aa512eb525611cd8186fa388bc06d1a37
mr-scrpt <mr.scrpt@gmail.com>
2020-09-05 16:15:14 +0300

    section first completed

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,44 @@
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Patch,
+  Delete,
+  Query,
+} from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+
+@Controller('coffees')
+export class CoffeesController {
+  constructor(private readonly coffeesServices: CoffeesService) {}
+
+  @Get()
+  findeAll(@Query() paginationQuery) {
+    //const { limit, offset } = paginationQuery;
+    return this.coffeesServices.findAll();
+  }
+
+  @Get(':id')
+  findeOne(@Param('id') id: string) {
+    return this.coffeesServices.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    return this.coffeesServices.create(createCoffeeDto);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+    return this.coffeesServices.update(id, updateCoffeeDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes #${id} coffee!`;
+  }
+}
